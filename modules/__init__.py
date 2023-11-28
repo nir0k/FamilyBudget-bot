1f8c987bea084a70a95e3e43418a1e51f0d9d92f
Ivan Korbakov <korbakovi@gmail.com>
2023-11-28 20:45:14 +0100

    refactoring code and add /cancel

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,5 @@
 """Modules package.
 
-Contains modules for handling various aspects of the application, such as 
+Contains modules for handling various aspects of the application, such as
 API, authentication, and utilities.
 """
